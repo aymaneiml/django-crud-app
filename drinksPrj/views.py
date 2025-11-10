d20dd872ed87969e541c7646e900a01fc072d431
aymaneiml <aymaneimlihi@gmail.com>
2025-11-10 20:51:56 +0100

    get,put by id

--- drinksPrj/views.py
+++ drinksPrj/views.py
@@ -24,3 +24,26 @@ def drink_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id):
+    try:
+        oneDrink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+    if request.method == 'GET':
+        serializer = DrinkSerializer(oneDrink)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        ##if not valid
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        pass
